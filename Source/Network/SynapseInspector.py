4246ee85d2e469382aecc398b6d205abf2d2a0ae
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-01-12 22:23:15 +0000

    Can now set excitatory vs. inhibitory.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@2164 86afb187-6117-0410-83e3-87b601379a31

--- Source/Network/SynapseInspector.py
+++ Source/Network/SynapseInspector.py
@@ -13,5 +13,28 @@ class SynapseInspector( ObjectInspector ):
         
         # Lazily create our UI
         if not hasattr(self, ''):
-#            self.GetSizer().Add(foo, 0, wx.EXPAND)
-            self.GetSizer().Layout()
+            gridSizer = wx.FlexGridSizer(2, 2, 8, 8)
+            
+            self.excitatoryButton = wx.RadioButton(self, wx.ID_ANY, _('Excitatory'), style=wx.RB_GROUP)
+            self.Bind(wx.EVT_RADIOBUTTON, self.onSetActivation, self.excitatoryButton)
+            gridSizer.Add(wx.StaticText(self, wx.ID_ANY, _('Activation:')), 0)
+            gridSizer.Add(self.excitatoryButton, 0)
+            self.inhibitoryButton = wx.RadioButton(self, wx.ID_ANY, _('Inhibitory'))
+            self.Bind(wx.EVT_RADIOBUTTON, self.onSetActivation, self.inhibitoryButton)
+            gridSizer.AddStretchSpacer()
+            gridSizer.Add(self.inhibitoryButton, 0)
+            
+            self.GetSizer().Add(gridSizer, 1)
+        
+        if self.objects.haveEqualAttr('excitatory'):
+            self.excitatoryButton.SetValue(self.objects[0].excitatory)
+            self.inhibitoryButton.SetValue(not self.objects[0].excitatory)
+        else:
+            self.excitatoryButton.SetValue(false)
+            self.inhibitoryButton.SetValue(false)
+        
+        self.Layout()
+    
+    
+    def onSetActivation(self, event):
+        newValue = self.excitatoryButton.GetValue()        for object in self.objects:            object.excitatory = newValue
