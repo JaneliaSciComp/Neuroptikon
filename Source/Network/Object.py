049392175cc716b999b7dd8a977190032b756759
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-01-13 21:48:32 +0000

    Removed verbose debug message.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@2173 86afb187-6117-0410-83e3-87b601379a31

--- Source/Network/Object.py
+++ Source/Network/Object.py
@@ -35,13 +35,12 @@ class Object(object):
     
     
     def __setattr__(self, name, value):
-        # Send out a message whenever one of this object's values changes.  This is needed, for example, to allow the GUI to update when an object is modified via the console.
+        # Send out a message whenever one of this object's values changes.  This is needed, for example, to allow the GUI to update when an object is modified via the console or a script.
         hasPreviousValue = hasattr(self, name)
         if hasPreviousValue:
             previousValue = getattr(self, name)
         object.__setattr__(self, name, value)
         if not hasPreviousValue or getattr(self, name) != previousValue:
-            print "Set " + name + " to " + str(value)
             dispatcher.send(('set', name), self)
     
     
