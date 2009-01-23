39c1e099f3903f9b363a64d0bff6cf3c87983a5f
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-01-23 22:29:58 +0000

    Added findTerms method and error checking.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@2199 86afb187-6117-0410-83e3-87b601379a31

--- Source/Library/Ontology.py
+++ Source/Library/Ontology.py
@@ -28,6 +28,8 @@ class Ontology(LibraryItem):
             elif entry.stanzaName == 'Term':
                 term = OntologyTerm(identifier = entry.id.value, name = entry.name.value)
                 self._termDict[term.identifier] = term
+                
+                # If the term has 'part-of' relationship then try to set the parent term.
                 parentId = None
                 for relationship in entry.relationship or []:
                     if relationship.relationship == 'part_of':
@@ -37,17 +39,25 @@ class Ontology(LibraryItem):
                             term.partOf = parent
                             parent.parts.append(term)
                         else:
+                            # The parent of this term has not been loaded yet.  Store its ID and look it up later.
                             term.partOf = parentId
                             unresolvedRefs.append(term)
+                if parentId is None:
+                    self.rootTerms.append(term)
+                
+                # Grab the abbreviation if there is one.
                 for synonym in entry.synonyms or []:
                     if 'ABBREVIATION' in synonym.types:
                         term.abbreviation = synonym.value
-                if parentId is None:
-                    self.rootTerms.append(term)
             else:
                 pass
         
+        # Set the parent of any terms that came before their parent in the file.
         for term in unresolvedRefs:
+            if term.partOf not in self._termDict:
+                self.rootTerms = []
+                self._termDict = {}
+                raise ValueError, _("The parent (%s) of term '%s' (%s) is not in the ontology.") % (term.partOf, term.name, term.identifier)
             parent = self._termDict[term.partOf]
             term.partOf = parent
             parent.parts.append(term)
@@ -60,14 +70,21 @@ class Ontology(LibraryItem):
             if self._termDict.has_key(identifier):
                 matchingTerm = self._termDict[identifier]
         else:
-            termsToMatch = list(self.rootTerms)
-            while len(termsToMatch) > 0:
-                term = termsToMatch[0]
-                termsToMatch[0:1] = []
-                termsToMatch.extend(term.parts)
+            for term in self._termDict.itervalues():
                 if (name is not None and name.upper() == term.name.upper()) or \
                    (abbreviation is not None and term.abbreviation is not None and abbreviation.upper() == term.abbreviation.upper()):
                     matchingTerm = term
                     break
         
         return matchingTerm
+    
+    
+    def findTerms(self, namePart = None, abbreviationPart = None):
+        matchingTerms = []
+        
+        for term in self._termDict.itervalues():
+            if (namePart is not None and term.name is not None and term.name.upper().find(namePart.upper()) != -1) or \
+               (abbreviationPart is not None and term.abbreviation is not None and term.abbreviation.upper().find(abbreviationPart.upper()) != -1):
+                matchingTerms.append(term)
+        
+        return matchingTerms
