6d226a4c835ec9f642c008d231bd5c4ee4e26a0d
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-01-27 16:00:07 +0000

    Tweaked ontology API.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@2214 86afb187-6117-0410-83e3-87b601379a31

--- Source/Library/Ontology.py
+++ Source/Library/Ontology.py
@@ -2,7 +2,7 @@ from LibraryItem import LibraryItem
 from OntologyTerm import OntologyTerm
 import obitools.obo.parser as obo
 
-class Ontology(LibraryItem):
+class Ontology(LibraryItem, dict):
     
     @classmethod
     def listProperty(cls):
@@ -17,7 +17,6 @@ class Ontology(LibraryItem):
     def __init__(self, identifier = None, *args, **keywordArgs):
         LibraryItem.__init__(self, identifier, *args, **keywordArgs)
         self.rootTerms = []
-        self._termDict = {}
     
     
     def importOBO(self, filePath):
@@ -26,55 +25,33 @@ class Ontology(LibraryItem):
             if entry.isHeader:
                 pass
             elif entry.stanzaName == 'Term':
-                term = OntologyTerm(identifier = entry.id.value, name = entry.name.value)
-                self._termDict[term.identifier] = term
+                term = OntologyTerm(self, oboStanza = entry)
+                self[term.identifier] = term
                 
-                # If the term has 'part-of' relationship then try to set the parent term.
-                parentId = None
-                for relationship in entry.relationship or []:
-                    if relationship.relationship == 'part_of':
-                        parentId = relationship.value
-                        if self._termDict.has_key(parentId):
-                            parent = self._termDict[parentId]
-                            term.partOf = parent
-                            parent.parts.append(term)
-                        else:
-                            # The parent of this term has not been loaded yet.  Store its ID and look it up later.
-                            term.partOf = parentId
-                            unresolvedRefs.append(term)
-                if parentId is None:
+                if term.partOf is None:
                     self.rootTerms.append(term)
-                
-                # Grab the abbreviation if there is one.
-                for synonym in entry.synonyms or []:
-                    if 'ABBREVIATION' in synonym.types:
-                        term.abbreviation = synonym.value
-            else:
-                pass
+                elif not isinstance(term.partOf, OntologyTerm):
+                    unresolvedRefs.append(term)
         
         # Set the parent of any terms that came before their parent in the file.
         for term in unresolvedRefs:
-            if term.partOf not in self._termDict:
+            if term.partOf not in self:
                 self.rootTerms = []
-                self._termDict = {}
+                self.clear()
                 raise ValueError, _("The parent (%s) of term '%s' (%s) is not in the ontology.") % (term.partOf, term.name, term.identifier)
-            parent = self._termDict[term.partOf]
+            parent = self[term.partOf]
             term.partOf = parent
             parent.parts.append(term)
     
     
-    def findTerm(self, identifier = None, name = None, abbreviation = None):
+    def findTerm(self, name = None, abbreviation = None):
         matchingTerm = None
         
-        if identifier is not None:
-            if self._termDict.has_key(identifier):
-                matchingTerm = self._termDict[identifier]
-        else:
-            for term in self._termDict.itervalues():
-                if (name is not None and name.upper() == term.name.upper()) or \
-                   (abbreviation is not None and term.abbreviation is not None and abbreviation.upper() == term.abbreviation.upper()):
-                    matchingTerm = term
-                    break
+        for term in self.itervalues():
+            if (name is not None and name.upper() == term.name.upper()) or \
+               (abbreviation is not None and term.abbreviation is not None and abbreviation.upper() == term.abbreviation.upper()):
+                matchingTerm = term
+                break
         
         return matchingTerm
     
@@ -82,7 +59,7 @@ class Ontology(LibraryItem):
     def findTerms(self, namePart = None, abbreviationPart = None):
         matchingTerms = []
         
-        for term in self._termDict.itervalues():
+        for term in self.itervalues():
             if (namePart is not None and term.name is not None and term.name.upper().find(namePart.upper()) != -1) or \
                (abbreviationPart is not None and term.abbreviation is not None and term.abbreviation.upper().find(abbreviationPart.upper()) != -1):
                 matchingTerms.append(term)
--- Source/Library/OntologyTerm.py
+++ Source/Library/OntologyTerm.py
@@ -1,10 +1,44 @@
+import Ontology
+
 class OntologyTerm(object):
 
-    def __init__(self, identifier = None, name = None, abbreviation = None, *args, **keywordArgs):
+    def __init__(self, ontology, oboStanza = None, *args, **keywordArgs):
         object.__init__(self, *args, **keywordArgs)
-        self.identifier = identifier
-        self.name = name
-        self.abbreviation = abbreviation
+        
+        if not isinstance(ontology, Ontology.Ontology):
+            raise ValueError, _('The first argument must be an Ontology instance')
+        
+        self.ontology = ontology
+        self.oboStanza = oboStanza
+        self.identifier = None
+        self.name = None
+        self.abbreviation = None
         self.partOf = None
         self.parts = []
+        self.obsolete = False
+        
+        if self.oboStanza is not None:
+            self.identifier = self.oboStanza.id.value
+            self.name = self.oboStanza.name.value
+            
+            # If this term has 'part-of' relationship then try to set the parent term.
+            for relationship in self.oboStanza.relationship or []:
+                if relationship.relationship == 'part_of':
+                    parentId = relationship.value
+                    if parentId in self.ontology:
+                        parentTerm = self.ontology[parentId]
+                        self.partOf = parentTerm
+                        parentTerm.parts.append(self)
+                    else:
+                        # The parent of this term has not been loaded yet.  Store its ID and look it up later.
+                        self.partOf = parentId
+            
+            # Grab any abbreviation.
+            for synonym in self.oboStanza.synonyms or []:
+                if 'ABBREVIATION' in synonym.types:
+                    self.abbreviation = synonym.value
+                # TODO: grab other synonyms?
+            
+            if self.oboStanza.definition is not None and self.oboStanza.definition.value == 'Obsolete.':
+               self.obsolete = True
     
