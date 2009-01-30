7a1a021536f42fbdd11b77afddb0608d0c4a7e6e
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-01-30 22:34:39 +0000

    Updated to new API.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@2238 86afb187-6117-0410-83e3-87b601379a31

--- Source/Scripts/Circle.py
+++ Source/Scripts/Circle.py
@@ -1,9 +1,9 @@
 count = 8
 neurons = {}
 for i in range(count):
-	neurons[i] = network.createNeuron(str(i))
+	neurons[i] = network.createNeuron(name = str(i))
 for i in range(count):
 	for j in range(count):
 		neurons[i].synapseOn(neurons[j])
-display.autoLayout('spectral-mitya')
-display.centerView()
+
+display.autoLayout()
