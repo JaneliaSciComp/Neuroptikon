dcdfc2de156575847312883f41688dfdbb3f4612
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-04-14 19:24:57 +0000

    Now packaging a completely standalone app.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@2571 86afb187-6117-0410-83e3-87b601379a31

--- Source/setup.py
+++ Source/setup.py
@@ -8,19 +8,22 @@ Usage (Windows):
     python setup.py py2exe
 """
 
-import sys
-from setuptools import setup
+import wxversion
+wxversion.select('2.8')
 
 app_scripts = ['Neuroptikon.py']
 setup_options = dict()
 
-resources = ['Images', 'Inspectors', 'Ontologies', 'Textures']
+resources = ['Images', 'Inspectors', 'Ontologies', 'Scripts', 'Textures']
 
+import sys
 if sys.platform == 'darwin':
 	setup_options['setup_requires'] = ['py2app']
 	setup_options['app'] = app_scripts
 	
 	py2app_options = dict()
+	
+	py2app_options['packages'] = ['wx']
 	
 	resources.append('lib/Darwin/fdp')
 	resources.append('lib/Darwin/graphviz')
@@ -39,7 +42,15 @@ elif sys.platform == 'win32':
 else:
 	pass	# TODO: UNIX/Linux systems
 
+from setuptools import setup
 setup(
 	name='Neuroptikon', 
 	**setup_options
 )
+
+# Strip out any .pyc or .pyo files.
+import os
+for root, dirs, files in os.walk('dist' + os.sep + 'Neuroptikon.app', topdown=False):
+    for name in files:
+    	if os.path.splitext(name)[1] in ['.pyc', '.pyo']:
+	        os.remove(os.path.join(root, name))
