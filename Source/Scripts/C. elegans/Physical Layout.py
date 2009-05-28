17c009361225cb24a7b7b6100fb0d25a15226917
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-05-28 18:04:30 +0000

    Tweaked coloring and positioning.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@2777 86afb187-6117-0410-83e3-87b601379a31

--- Source/Scripts/C. elegans/Physical Layout.py
+++ Source/Scripts/C. elegans/Physical Layout.py
@@ -30,13 +30,14 @@ neuronTypesFile.close()
 display.setViewDimensions(2)
 display.setShowNeuronNames(True)
 display.setUseGhosts(True)
+display.setDefaultFlowSpread(0.5)
 
 ACh = library.neurotransmitter('ACh')
 GABA = library.neurotransmitter('GABA')
 
 for neuron in display.network.neurons():
     # Color the soma based on function.
-    red = green = blue = 0.0
+    red = green = blue = 0.5
     if neuron.hasFunction(NeuralFunction.SENSORY):
         red = 1.0
     if neuron.hasFunction(NeuralFunction.INTERNEURON):
@@ -51,28 +52,28 @@ for neuron in display.network.neurons():
     somaSide = neuron.getAttribute('Side')
     if somaSide is not None:
         if somaSide.value == 'L':
-            somaY = -0.1
+            somaY = -0.15
         elif somaSide.value == 'R':
-            somaY = 0.1
+            somaY = 0.15
     # Many soma have the exact same X/Y coordinates so we distribute them evenly around the common point.
     clusterKey = str(somaPositions[neuron.name]) + neuron.getAttribute('Side').value
     cluster = clusters[clusterKey]
-    somaY += (len(cluster) - 1) / 2.0 * 0.011 - cluster.index(neuron.name) * 0.011
-    display.setVisiblePosition(neuron, (somaX, somaY, 0.0))
+    somaY += (len(cluster) - 1) / 2.0 * 0.015 - cluster.index(neuron.name) * 0.015
+    display.setVisiblePosition(neuron, (somaX, somaY, 0.0), fixed = True)
 
 # Color the synapses according to excitation/inhibition and weight them according to their connection count.
 for synapse in display.network.synapses():
     if ACh in synapse.preSynapticNeurite.neuron().neurotransmitters:
-        display.setVisibleColor(synapse, (0.5, 0.5, 1.0))
+        display.setVisibleColor(synapse, (0.5, 0.5, 0.75))
     elif GABA in synapse.preSynapticNeurite.neuron().neurotransmitters:
-        display.setVisibleColor(synapse, (1.0, 0.5, 0.5))
+        display.setVisibleColor(synapse, (0.75, 0.5, 0.5))
     else:
         display.setVisibleColor(synapse, (0.5, 0.5, 0.5))
-    display.setVisibleWeight(synapse, 0.5 if synapse.getAttribute('Count').value < 5 else 1.0)
+    display.setVisibleWeight(synapse, 0.25 if synapse.getAttribute('Count').value < 5 else 0.5)
 
-# Make all gap junctions gray and weight them according to their connection count.
+# Make all gap junctions black and weight them according to their connection count.
 for gapJunction in display.network.gapJunctions():
     display.setVisibleColor(gapJunction, (0.0, 0.0, 0.0))
-    display.setVisibleWeight(gapJunction, 0.5 if gapJunction.getAttribute('Count').value < 5 else 1.0)
+    display.setVisibleWeight(gapJunction, 0.25 if gapJunction.getAttribute('Count').value < 5 else 0.5)
 
 display.centerView()
