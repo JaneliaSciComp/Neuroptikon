81dfbd1a0c739fd933fb65c412d3aaa32da8e7e3
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-07-30 21:41:54 +0000

    Removed duplicate visualizations. Added innervations.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@3043 86afb187-6117-0410-83e3-87b601379a31

--- Source/Scripts/C. elegans/Locomotion.py
+++ Source/Scripts/C. elegans/Locomotion.py
@@ -2,55 +2,93 @@ display.autoVisualize = False
 display.setShowNeuronNames(True)
 display.setUseGhosts(True)
 
-if not any(network.neurons()):
-    execfile('Neurons.py')
+if not any(network.muscles()):
+    execfile('Muscles.py')
 
 neurons = []
+muscles = []
+gapJunctions = []
 
 verticalSpacing = 0.15
 
 for index in range(1, 8):
     neuron = network.findNeuron('DB%02d' % index)
-    display.visualizeObject(neuron, position = (1.0 / 8.0 * index - 0.5, verticalSpacing * 1.5, 0.0), positionIsFixed = True)
+    display.visualizeObject(neuron, position = (1.0 / 8.0 * index - 0.5, verticalSpacing * 1.5, 0.0), positionIsFixed = True, size = (0.015, 0.015, 0.015))
     neurons.append(neuron)
 
 for index in range(1, 14):
     neuron = network.findNeuron('VD%02d' % index)
-    display.visualizeObject(neuron, position = (1.0 / 14.0 * index - 0.5, verticalSpacing * 0.5, 0.0), positionIsFixed = True)
+    display.visualizeObject(neuron, position = (1.0 / 14.0 * index - 0.5, verticalSpacing * 0.5, 0.0), positionIsFixed = True, size = (0.015, 0.015, 0.015))
     neurons.append(neuron)
 
 for index in range(1, 7):
     neuron = network.findNeuron('DD%02d' % index)
-    display.visualizeObject(neuron, position = (1.0 / 7.0 * index - 0.5, verticalSpacing * -0.5, 0.0), positionIsFixed = True)
+    display.visualizeObject(neuron, position = (1.0 / 7.0 * index - 0.5, verticalSpacing * -0.5, 0.0), positionIsFixed = True, size = (0.015, 0.015, 0.015))
     neurons.append(neuron)
 
 for index in range(1, 12):
     neuron = network.findNeuron('VB%02d' % index)
-    display.visualizeObject(neuron, position = (1.0 / 12.0 * index - 0.5, verticalSpacing * -1.5, 0.0), positionIsFixed = True)
+    display.visualizeObject(neuron, position = (1.0 / 12.0 * index - 0.5, verticalSpacing * -1.5, 0.0), positionIsFixed = True, size = (0.015, 0.015, 0.015))
     neurons.append(neuron)
 
 ACh = library.neurotransmitter('ACh')
 GABA = library.neurotransmitter('GABA')
 
 for neuron in neurons:
-    for synapse in neuron.incomingSynapses():
-        if synapse.preSynapticNeurite.neuron() in neurons:
-            display.visualizeObject(synapse)
-            if ACh in synapse.preSynapticNeurite.neuron().neurotransmitters:
-                display.setVisibleColor(synapse, (0.5, 0.5, 1.0))
-            elif GABA in synapse.preSynapticNeurite.neuron().neurotransmitters:
-                display.setVisibleColor(synapse, (1.0, 0.5, 0.5))
+    # Color the soma based on function.
+    shapeColor = [0.5, 0.5, 0.5]
+    labelColor = [0.0, 0.0, 0.0]
+    if neuron.hasFunction(NeuralFunction.SENSORY):
+        shapeColor[0] = 1.0
+        labelColor[0] = 0.125
+    if neuron.hasFunction(NeuralFunction.INTERNEURON):
+        shapeColor[2] = 1.0
+        labelColor[2] = 0.125
+    if neuron.hasFunction(NeuralFunction.MOTOR):
+        shapeColor[1] = 1.0
+        labelColor[1] = 0.125
+    display.setVisibleColor(neuron, shapeColor)
+    display.setLabelColor(neuron, labelColor)
+    
+    # Visualize any chemical synapses that this neuron makes onto other neurons in the visualization.
     for synapse in neuron.outgoingSynapses():
         if synapse.postSynapticNeurites[0].neuron() in neurons:
-            display.visualizeObject(synapse)
+            display.visualizeObject(synapse, weight = 2.0)
             if ACh in neuron.neurotransmitters:
                 display.setVisibleColor(synapse, (0.5, 0.5, 1.0))
             elif GABA in neuron.neurotransmitters:
                 display.setVisibleColor(synapse, (1.0, 0.5, 0.5))
+    
+    # Visualize any gap junctions that this neuron makes with other neurons in the visualization.
     for gapJunction in neuron.gapJunctions():
-        neurites = list(gapJunction.neurites)
-        if (neurites[0].neuron() == neuron and neurites[1].neuron() in neurons) or (neurites[1].neuron() == neuron and neurites[0].neuron() in neurons):
-            display.visualizeObject(gapJunction)
+        for neurite in gapJunction.neurites:
+            if neurite.neuron() != neuron:
+                otherNeuron = neurite.neuron()
+        if gapJunction not in gapJunctions and otherNeuron in neurons:
+            display.visualizeObject(gapJunction, weight = 2.0)
             display.setVisibleColor(gapJunction, (0.5, 0.5, 0.5))
-
+            gapJunctions += [gapJunction]
+    
+    # Visualize any innervations that this neuron makes.
+    for innervation in neuron.innervations():
+        muscle = innervation.muscle
+        if muscle not in muscles:
+            muscleX = muscle.getAttribute('A-P Position').value * 1.5 - 0.75
+            muscleY = 0.0
+            muscleSide = muscle.getAttribute('Side')
+            if muscleSide is not None:
+                if muscleSide.value == 'L':
+                    muscleY = -0.3
+                elif muscleSide.value == 'R':
+                    muscleY = 0.3
+            muscleFace = muscle.getAttribute('Face')
+            if muscleFace is not None:
+                if muscleFace.value == 'D':
+                    muscleY -= 0.025
+                elif muscleFace.value == 'V':
+                    muscleY += 0.025
+            display.visualizeObject(muscle, position = (muscleX, muscleY, 0.0), size = (0.01, 0.02, 0.01))
+            muscles += [muscle]
+        display.visualizeObject(innervation, weight = 0.5, opacity = 0.5)
+    
 display.centerView()
