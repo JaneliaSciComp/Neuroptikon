d56cd94d4207e8662d5d49244ea13ef04d053aa6
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-09-18 21:53:45 +0000

    New sample scripts for visualizing morphology.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@3224 86afb187-6117-0410-83e3-87b601379a31

---
+++ Source/Scripts/Morphology.py
@@ -0,0 +1,36 @@
+'''
+A very simple example of neural morphology.
+
+A neuron is created with a single process that branches multiple times to form two separate arbors and synapse onto a second neuron.
+'''
+
+neuron1 = network.createNeuron(name = '1')
+neuron2 = network.createNeuron(name = '2')
+regionA = network.createRegion(name = 'A')
+regionB = network.createRegion(name = 'B')
+
+# Create the root of the process.
+neurite1 = neuron1.createNeurite()
+
+# The process first branches to arborize region A.
+neurite2 = neurite1.createNeurite()
+neurite2.arborize(regionA, sendsOutput = True)
+neurite3 = neurite1.createNeurite()
+
+# The process then branches again to arborize region B.
+neurite4 = neurite3.createNeurite()
+neurite4.arborize(regionB, sendsOutput = True)
+
+# The process branches a final time and synapses on the second neuron.
+neurite5 = neurite3.createNeurite()
+neurite5.synapseOn(neuron2)
+
+# By default the morphology of the process is not shown, the arborizations and synapse in the visualization will connect directly to the neuron's soma.
+# The 'Show Neurites' scripts exposes the morphology.
+execfile('Show Neurites.py')
+
+# Now layout the whole mess and make the flow look better than the defaults.
+display.performLayout()
+display.setDefaultFlowSpacing(0.02)
+display.setDefaultFlowSpeed(0.02)
+display.setShowNeuronNames(True)
---
+++ Source/Scripts/Show Neurites.py
@@ -0,0 +1,97 @@
+'''
+Visualize any neurites that are not dedicated to a single connection, e.g. those created by the convenience methods like Neuron.synapseOn() or Neurite.arborize().
+
+This functionality will eventually be folded into Neuroptikon. 
+'''
+
+
+def visualizeNeurite(neurite):
+    '''
+    Visualize the indicated neurite and any neurites between it and the soma if they are not already visualized.
+    
+    Returns the visible proxy of the neurite that was created or that already existed.
+    '''
+    
+    visibles = display.visiblesForObject(neurite)
+    if any(visibles):
+        return visibles[0]
+    else:
+        root = neurite.root
+        if isinstance(root, Neuron):
+            startVisible = display.visiblesForObject(root)[0]
+        else:
+            rootVisible = visualizeNeurite(root)
+            startVisible = rootVisible.pathEndPoints()[1]
+        neuriteVisible = display.visualizeObject(neurite)
+        branchPointVisible = display.visualizeObject(None, opacity = 0.0, size = (0.005, 0.005, 0.005))
+        neuriteVisible.setPathEndPoints(startVisible, branchPointVisible)
+        # TODO: set the end point of everything else downstream of the branch point? 
+        return neuriteVisible
+
+
+def updateFlow(neuriteVisible, flowTo = False, flowFrom = False):
+    '''
+    Update the information flow back towards the neuron's soma.
+    
+    If a neurite has information flow then it is assumed that the flow travels all to the way to or from the soma.  This may not be biologically accurate in all cases.
+    '''
+    
+    if flowTo:
+        neuriteVisible.setFlowTo(True)
+    if flowFrom:
+        neuriteVisible.setFlowFrom(True)
+    root = neuriteVisible.client.root
+    if isinstance(root, Neurite):
+        rootVisible = display.visiblesForObject(root)[0]
+        updateFlow(rootVisible, flowTo = flowTo, flowFrom = flowFrom)
+
+
+if any(display.selection()):
+    arborizations = []
+    synapses = []
+    for object in display.selectedObjects():
+        if isinstance(object, Arborization):
+            arborizations += [object]
+        elif isinstance(object, Synapse):
+            synapses += [object]
+else:
+    arborizations = network.arborizations()
+    synapses = network.synapses()
+
+# Visualize any additional neurites between an arborization's neurite and its neuron.
+for arborization in arborizations:
+    root = arborization.neurite.root
+    if not isinstance(root, Neuron):
+        visibles = display.visiblesForObject(arborization)
+        if any(visibles):
+            arborizationPath = visibles[0]
+            neuriteVisible = visualizeNeurite(arborization.neurite.root)
+            if arborization.sendsOutput:
+                updateFlow(neuriteVisible, flowTo = True)
+            if arborization.receivesInput:
+                updateFlow(neuriteVisible, flowFrom = True)
+            arborizationPath.setPathEndPoints(neuriteVisible.pathEndPoints()[1], arborizationPath.pathEndPoints()[1])
+
+
+# Visualize any additional neurites connecting to synapses.
+for synapse in synapses:
+    visibles = display.visiblesForObject(synapse)
+    if any(visibles):
+        synapsePath = visibles[0]
+        synapseStart, synapseEnd = synapsePath.pathEndPoints()
+        preRoot = synapse.preSynapticNeurite.root
+        if not isinstance(preRoot, Neuron):
+            preSynapticPath = visualizeNeurite(preRoot)
+            updateFlow(preSynapticPath, flowTo = True)
+            synapseStart = preSynapticPath.pathEndPoints()[1]
+        postRoot = synapse.postSynapticNeurites[0].root
+        if not isinstance(postRoot, Neuron):
+            postSynapticPath = visualizeNeurite(postRoot)
+            updateFlow(postSynapticPath, flowFrom = True)
+            synapseEnd = postSynapticPath.pathEndPoints()[0]
+        synapsePath.setPathEndPoints(synapseStart, synapseEnd)
+        # TODO: handle synapses with multiple post-synaptic neurites
+
+
+# TODO: gap junctions
+# TODO: innervations
