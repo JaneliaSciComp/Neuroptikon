2125ec50416ddebd5fcb87fbd77cb6c3026a1150
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-10-06 18:08:41 +0000

    self._texture is now guaranteed to be defined.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@3309 86afb187-6117-0410-83e3-87b601379a31

--- Source/Library/Texture.py
+++ Source/Library/Texture.py
@@ -27,6 +27,12 @@ class Texture(LibraryItem):
         return bitmap
     
     
+    def __init__(self, *args, **keywordArgs):
+        LibraryItem.__init__(self, *args, **keywordArgs)
+        
+        self._texture = None
+    
+    
     def loadImage(self, imagePath):
         try:
             image = osgDB.readImageFile(str(imagePath))
@@ -34,12 +40,12 @@ class Texture(LibraryItem):
             image = None
         if image is not None and image.valid():
             self._texture = osg.Texture2D()
-            self._texture.setFilter(osg.Texture2D.MIN_FILTER, osg.Texture2D.LINEAR);
-            self._texture.setFilter(osg.Texture2D.MAG_FILTER, osg.Texture2D.LINEAR);
+            self._texture.setFilter(osg.Texture2D.MIN_FILTER, osg.Texture2D.LINEAR)
+            self._texture.setFilter(osg.Texture2D.MAG_FILTER, osg.Texture2D.LINEAR)
             self._texture.setImage(image)
-            self._texture.setWrap(osg.Texture2D.WRAP_S,  osg.Texture2D.REPEAT)
-            self._texture.setWrap(osg.Texture2D.WRAP_T,  osg.Texture2D.REPEAT)
-            self._texture.setWrap(osg.Texture2D.WRAP_R,  osg.Texture2D.REPEAT)
+            self._texture.setWrap(osg.Texture2D.WRAP_S, osg.Texture2D.REPEAT)
+            self._texture.setWrap(osg.Texture2D.WRAP_T, osg.Texture2D.REPEAT)
+            self._texture.setWrap(osg.Texture2D.WRAP_R, osg.Texture2D.REPEAT)
         else:
             self._texture = None
         
