a2bc0e27d687930e3633cea63fa026929fd7b811
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-10-16 15:45:55 +0000

    Now keeping muscles out of the initial display.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@3350 86afb187-6117-0410-83e3-87b601379a31

--- Source/Scripts/C. elegans/Dual Display.py
+++ Source/Scripts/C. elegans/Dual Display.py
@@ -5,9 +5,15 @@ This script demonstrates opening two visualizations of the same network.
 # Open the "connectivity" visualization in the current display.
 execfile('Connectivity.py')
 
+# Don't show the muscles in the original display
+display.autoVisualize = False
+
 # Open the "physical layout" visualization in a second display.
 scriptGlobals = globals()
 scriptGlobals['display'] = displayNetwork(network)
 execfile('Physical Layout.py', scriptGlobals, locals())
+
+# Re-enable auto-visualization in case anything else gets added by the user.
+display.autoVisualize = True
 
 # By default the selections of the two display will be synchronized.
