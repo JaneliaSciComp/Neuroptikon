76587b9b7ac030b53adf1375a36a4ebf6d2d2859
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-10-16 15:30:26 +0000

    Tweaked dimensions of mid-points for loop paths.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@3349 86afb187-6117-0410-83e3-87b601379a31

--- Source/Display/Visible.py
+++ Source/Display/Visible.py
@@ -1959,9 +1959,11 @@ class Visible(object):
             # Once mid-points become editable in the GUI the temporary mid-points should become permanent once modified by the user.
             center = path[0]
             size = self._pathStart.worldSize()
-            pad1 = (sum(size) / len(size)) / 2.0 * 1.1
-            pad2 = (sum(size) / len(size)) / 2.0 * 1.5
-            path += [(center[0] + pad1, center[1], center[2]), (center[0] + pad2, center[1], center[2]), (center[0] + pad2, center[1] + pad2, center[2]), (center[0], center[1] + pad2, center[2]), (center[0], center[1] + pad1, center[2]), center]
+            halfSize = (size[0] / 2.0, size[1] / 2.0, size[2] / 2.0)
+            minHalfSize = min(halfSize)
+            pad1 = minHalfSize * 0.1
+            pad2 = minHalfSize * 0.5
+            path += [(center[0] + halfSize[0] + pad1, center[1], center[2]), (center[0] + halfSize[0] + pad2, center[1], center[2]), (center[0] + halfSize[0] + pad2, center[1] + halfSize[1] + pad2, center[2]), (center[0], center[1] + halfSize[1] + pad2, center[2]), (center[0], center[1] + halfSize[1] + pad1, center[2]), center]
         else:
             path += [self._pathEnd.worldPosition()]
         
