16818a30cdf1cdd07b8ed35ac2c6eba02c808904
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-11-10 20:12:46 +0000

    Now only removing muscles that are not connected to any neurons. Now resetting size and opacity to defaults in case the centrality script has been run.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@3389 86afb187-6117-0410-83e3-87b601379a31

--- Source/Scripts/C. elegans/Connectivity.py
+++ Source/Scripts/C. elegans/Connectivity.py
@@ -12,9 +12,14 @@ display.setDefaultFlowSpread(0.5)
 if not any(network.neurons()):
     execfile('Neurons.py')
 
-# Remove the muscles if they were added by another script.
+# Remove unconnected muscles that would cause problems with the layout.
 for muscle in network.muscles():
-    display.removeObject(muscle)
+    removeMuscle = True
+    for innervation in muscle.innervations():
+        if any(display.visiblesForObject(innervation)):
+            removeMuscle = False 
+    if removeMuscle:
+        display.removeObject(muscle)
 
 # Set up the visualization
 
@@ -29,14 +34,25 @@ for neuron in display.network.neurons():
     display.setVisibleColor(neuron, (red, green, blue))
     display.setLabelColor(neuron, (red * 0.125, green * 0.125, blue * 0.125))
     display.setVisiblePosition(neuron, fixed = True)
+    display.setVisibleSize(neuron, (.01, .01, .01))
+    display.setVisibleOpacity(neuron, 1.0)
 
 # Make all chemical synapses light gray.
 for synapse in display.network.synapses():
     display.setVisibleColor(synapse, (0.75, 0.75, 0.75))
+    display.setVisibleOpacity(synapse, 1.0)
 
 # Make all gap junctions dark green.
 for gapJunction in display.network.gapJunctions():
     display.setVisibleColor(gapJunction, (0.0, 0.75, 0.0))
+    display.setVisibleOpacity(gapJunction, 1.0)
+
+# Reset the size and opacity of muscles and innervations in case the centrality script has been run.
+for muscle in network.muscles():
+    display.setVisibleSize(muscle, (.05, .1, .02))
+    display.setVisibleOpacity(innervation, 1.0)
+for innervation in display.network.innervations():
+    display.setVisibleOpacity(innervation, 1.0)
 
 # Perform a spectral layout weighted by the 'Count' attribute of the synapses and gap junctions.
 def weightByCount(edgeVisible):
