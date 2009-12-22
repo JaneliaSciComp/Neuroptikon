c9e1d76f9656c44f270f08344f90e0c36fe27e34
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2009-12-22 16:37:27 +0000

    Now creating a simplified graph to speed centrality calculation.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@3537 86afb187-6117-0410-83e3-87b601379a31

--- Source/Scripts/C. elegans/Centrality.py
+++ Source/Scripts/C. elegans/Centrality.py
@@ -4,65 +4,71 @@
 #  http://license.janelia.org/license/jfrc_copyright_1_1.html
 
 """
-A script to demonstrate how to use the centrality algorithms built into NetworkX.
-See <http://networkx.lanl.gov/reference/algorithms.centrality.html> for the full list of algorithms and their descriptions.
+A custom centrality script for the C. elegans network.
 """
 
 # Load the neurons and their interconnections if needed.
 if not any(network.objects):
     execfile('Connectivity.py')
 
+# Create a simplified graph so the calculation doesn't take as long.  Weighted betweenness centrality is nearly an n-cubed algorithm: O(n m + n^2 log n).
 import networkx
-
-# Create a new graph with only a single edge between nodes weighted by the sum of the connection counts.
-# This only works with the C. elegans.py network at this point.  Other networks will have constant (1.0) edge weights.
-graph = networkx.XGraph()
-for (node1, node2, object) in network.graph.edges():
-    weight = 1.0
-    if object != None:
-        count = object.getAttribute('Count')
-        if count:
-            weight = count.value()
-    # Gap junctions are undirected which is represented in the original graph by two directed edges.  Divide the weight in half so the strength of the connection is correct.
-    if isinstance(object, GapJunction):
-        weight /= 2.0
-    if graph.has_edge(node1, node2):
-        previousWeight = 1.0 / graph.get_edge(node1, node2)
+def addEdge(graph, object1, object2, weight):
+    node1 = object1.networkId
+    node2 = object2.networkId
+    if node1 in graph and node2 in graph[node1]:
+        if weight < graph[node1][node2]['weight']:
+            # Use a smaller weight for an existing edge.
+            graph[node1][node2]['weight'] = weight
     else:
-        previousWeight = 0.0
-    graph.add_edge(node1, node2, 1.0 / (previousWeight + weight))   # the more connections the lower the weight
-    
-# Compute the centrality of each node in the graph. (uncomment one of the following)
-#centralities = networkx.degree_centrality(graph)
-#centralities = networkx.closeness_centrality(graph, weighted_edges = True)
-centralities = networkx.betweenness_centrality(graph, weighted_edges = True)
-#centralities = networkx.load_centrality(graph, weighted_edges = True)
-    
-# Compute the maximum centrality so we can normalize.
-maxCentrality = max(centralities.itervalues())
-
-# Alter the visualization of each node based on its centrality.
-objectCentralities = {}
-for node, centrality in centralities.iteritems():
-    object = network.objectWithId(node)
-    objectCentralities[object] = centrality / maxCentrality
-    diameter = 0.001 + objectCentralities[object] * 0.029
-    display.setVisibleSize(object, [diameter] * 3)
-
+        # Create a new edge.
+        graph.add_edge(node1, node2, weight = weight)
+simplifiedGraph = networkx.DiGraph()
 for synapse in network.synapses():
-    centrality = objectCentralities[synapse.preSynapticNeurite.neuron()]
     for neurite in synapse.postSynapticNeurites:
-        centrality += objectCentralities[neurite.neuron()]
-    centrality /= 1 + len(synapse.postSynapticNeurites)
-    display.setVisibleOpacity(synapse, centrality)
-
+        addEdge(simplifiedGraph, synapse.preSynapticNeurite.neuron(), neurite.neuron(), network.weightOfObject(synapse))
 for gapJunction in network.gapJunctions():
-    centrality = 0.0
-    for neurite in gapJunction.neurites():
-        centrality += objectCentralities[neurite.neuron()]
-    centrality /= 2.0
-    display.setVisibleOpacity(gapJunction, centrality)
-
+    neurites = gapJunction.neurites()
+    addEdge(simplifiedGraph, neurites[0].neuron(), neurites[1].neuron(), network.weightOfObject(gapJunction))
+    addEdge(simplifiedGraph, neurites[1].neuron(), neurites[0].neuron(), network.weightOfObject(gapJunction))
 for innervation in network.innervations():
-    centrality = (objectCentralities[innervation.neurite.neuron()] + objectCentralities[innervation.muscle]) / 2.0
-    display.setVisibleOpacity(innervation, centrality)
+    addEdge(simplifiedGraph, innervation.neurite.neuron(), innervation.muscle, network.weightOfObject(innervation))
+
+def progressCallback(fraction_complete = None):
+    return updateProgress('Calculating centrality...', fraction_complete)
+
+# Compute the centrality of each node in the graph. (uncomment one of the following)
+#centralities = networkx.degree_centrality(simplifiedGraph)
+#centralities = networkx.closeness_centrality(simplifiedGraph, weighted_edges = True)
+centralities = networkx.betweenness_centrality(simplifiedGraph, weighted_edges = True, progress_callback = progressCallback)
+#centralities = networkx.load_centrality(simplifiedGraph, weighted_edges = True)
+
+if any(centralities):
+    # Compute the maximum centrality so we can normalize.
+    maxCentrality = max(centralities.itervalues())
+    
+    # Alter the visualization of each node based on its centrality.
+    objectCentralities = {}
+    for node, centrality in centralities.iteritems():
+        object = network.objectWithId(node)
+        objectCentralities[object] = centrality / maxCentrality
+        diameter = 0.001 + objectCentralities[object] * 0.029
+        display.setVisibleSize(object, [diameter] * 3)
+    
+    for synapse in network.synapses():
+        centrality = objectCentralities[synapse.preSynapticNeurite.neuron()]
+        for neurite in synapse.postSynapticNeurites:
+            centrality += objectCentralities[neurite.neuron()]
+        centrality /= 1 + len(synapse.postSynapticNeurites)
+        display.setVisibleOpacity(synapse, centrality)
+    
+    for gapJunction in network.gapJunctions():
+        centrality = 0.0
+        for neurite in gapJunction.neurites():
+            centrality += objectCentralities[neurite.neuron()]
+        centrality /= 2.0
+        display.setVisibleOpacity(gapJunction, centrality)
+    
+    for innervation in network.innervations():
+        centrality = (objectCentralities[innervation.neurite.neuron()] + objectCentralities[innervation.muscle]) / 2.0
+        display.setVisibleOpacity(innervation, centrality)
