60a86a4b99a2e4dc290f45e5f4f24198e7657509
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2010-01-07 15:35:29 +0000

    Now making sure community.py gets packaged.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@3591 86afb187-6117-0410-83e3-87b601379a31

--- Source/setup.py
+++ Source/setup.py
@@ -26,7 +26,7 @@ app_scripts = ['neuroptikon.py']
 
 resources = ['Images', 'Inspectors', 'Layouts', 'Neuroptikon_v1.0.xsd', 'Ontologies', 'Shapes', 'Textures']
 resources += ['display/flow_shader.vert', 'display/flow_shader.frag', 'display/cull_faces.osg']
-includes = ['wx', 'xlrd']
+includes = ['community', 'wx', 'xlrd']
 excludes = ['Inspectors', 'Layouts', 'matplotlib', 'pygraphviz', 'scipy', 'Shapes']
 
 sys.path += ['lib/CrossPlatform', 'lib/' + platform.system()]
