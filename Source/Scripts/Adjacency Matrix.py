79227ebd4200321e1de01999aee2063d53c25245
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2010-02-18 22:17:48 +0000

    Now asking the user whether the connectivity represents pathways, synapses or gap junctions.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@3714 86afb187-6117-0410-83e3-87b601379a31

--- Source/Scripts/Adjacency Matrix.py
+++ Source/Scripts/Adjacency Matrix.py
@@ -5,35 +5,75 @@
 
 
 """
-This script loads a region-level network from a user-chosen adjacency matrix.
+This script loads a region- or neuron-level network from a user-chosen adjacency matrix.
 
-It is assumed that the first line of the file indicates the region name for each column (and thus for each row as well) 
-and that subsequent lines have the same number of tab separated columns with non-zero entries indicating adjacency.
+It is assumed that the first line of the file indicates the region/neuron name for each column (and thus for each row as well) 
+and that subsequent lines have the same number of tab separated columns with non-zero entries indicating adjacency.  The non-zero 
+value is assumed to indicate the strength of the connection with higher values indicating higher strengths.
 """
 
 import wx
 
+
+# Set up a weighting function that uses our custom "Count" attribute
+def weightByCount(networkObject):
+    countAttribute = networkObject.getAttribute('Count')
+    return 1.0 if not countAttribute else 1.0 / countAttribute.value()
+
+
 # Ask the user which adjacency file should be loaded.
-dlg = wx.FileDialog(None, 'Choose an adjacency file to load', '', '', '*.txt', wx.OPEN)
+fileDlg = wx.FileDialog(None, 'Choose an adjacency file to load', '', '', '*.txt', wx.OPEN)
 try:
-    if dlg.ShowModal() == wx.ID_OK:
-        adjancencyFile = open(dlg.GetPath())
-        regions = {}
+    if fileDlg.ShowModal() == wx.ID_OK:
+        # Ask the user what kind of connections are in the file.
+        choiceDlg = wx.SingleChoiceDialog(None, 'What type of connections are in the file?', 'Adjacency Matrix Loader', ['Pathways', 'Chemical synapses', 'Electrical synapses'])
         try:
-            # Create the regions.
-            regionNames = adjancencyFile.next().strip().split('\t')
-            for regionName in regionNames:
-                regions[regionName] = network.createRegion(name = regionName)
-            
-            # Create the pathways.
-            for preRegion, postRegions in map(None, regionNames, adjancencyFile):
-                if preRegion is not None and postRegions is not None:
-                    for postRegion, adjacency in map(None, regionNames, postRegions.strip().split('\t')):
-                        if postRegion is not None and adjacency is not None and adjacency.strip() != '0':
-                            regions[preRegion.strip()].projectToRegion(regions[postRegion.strip()])
-            
-            display.performLayout()
+            if choiceDlg.ShowModal() == wx.ID_OK:
+                connectionType = choiceDlg.GetStringSelection()
+                if connectionType == 'Pathways':
+                    regions = {}
+                else:
+                    neurons = {}
+                
+                # ...
+                network.setWeightingFunction(weightByCount)
+                
+                
+                adjancencyFile = open(fileDlg.GetPath())
+                try:
+                    # Create the regions/neurons.
+                    names = adjancencyFile.next().strip().split('\t')
+                    if connectionType == 'Pathways':
+                        for regionName in names:
+                            regions[regionName] = network.findRegion(name = regionName) or network.createRegion(name = regionName)
+                    else:
+                        for neuronName in names:
+                            neurons[neuronName] = network.findNeuron(name = neuronName) or network.createNeuron(name = neuronName)
+                    
+                    # Create the connections.
+                    for preObject, postObjects in map(None, names, adjancencyFile):
+                        if preObject is not None and postObjects is not None:
+                            for postObject, adjacency in map(None, names, postObjects.strip().split('\t')):
+                                if postObject is not None and adjacency is not None:
+                                    adjacency = adjacency.strip()
+                                    if adjacency != '0':
+                                        if connectionType == 'Pathways':
+                                            connection = regions[preObject.strip()].projectToRegion(regions[postObject.strip()])
+                                        elif connectionType == 'Chemical synapses':
+                                            connection = neurons[preObject.strip()].synapseOn(neurons[postObject.strip()])
+                                        elif connectionType == 'Electrical synapses':
+                                            connection = neurons[preObject.strip()].gapJunctionWith(neurons[postObject.strip()])
+                                        try:
+                                            connection.addAttribute('Count', Attribute.INTEGER_TYPE, int(adjacency))
+                                        except:
+                                            pass
+                    
+                    display.performLayout()
+                    if connectionType != 'Pathways':
+                        display.showNeuronNames()
+                finally:
+                    adjancencyFile.close()
         finally:
-            adjancencyFile.close()
+            choiceDlg.Destroy()
 finally:
-    dlg.Destroy()
+    fileDlg.Destroy()
