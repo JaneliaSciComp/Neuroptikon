8329d8b840283729a6f07e8795c605bb5c1800ca
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2010-02-23 17:49:20 +0000

    Updated version to 0.9.9.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@3743 86afb187-6117-0410-83e3-87b601379a31

--- Source/__version__.py
+++ Source/__version__.py
@@ -3,6 +3,6 @@
 #  Use is subject to Janelia Farm Research Campus Software Copyright 1.1 license terms.
 #  http://license.janelia.org/license/jfrc_copyright_1_1.html
 
-version = "0.9.8"
-version_info = (0, 9, 8)
-JIRA_version_id = 10001
+version = "0.9.9"
+version_info = (0, 9, 9)
+JIRA_version_id = 10011
