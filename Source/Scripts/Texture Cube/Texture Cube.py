f4a661ec36349e5ada892b1bc8d48241ee77d510
midgleyf <midgleyf@86afb187-6117-0410-83e3-87b601379a31>
2011-02-24 22:34:49 +0000

    Added copyright and description.
    git-svn-id: https://subversion/ScientificComputing/Projects/Neuroptikon/Trunk@5908 86afb187-6117-0410-83e3-87b601379a31

--- Source/Scripts/Texture Cube/Texture Cube.py
+++ Source/Scripts/Texture Cube/Texture Cube.py
@@ -1,3 +1,13 @@
+#  Copyright (c) 2011 Howard Hughes Medical Institute.
+#  All rights reserved.
+#  Use is subject to Janelia Farm Research Campus Software Copyright 1.1 license terms.
+#  http://license.janelia.org/license/jfrc_copyright_1_1.html
+
+"""
+A script to demonstrate 3D textures.
+Earth images from <http://www.randelshofer.ch/rubik/virtualcubes/virtual_rubik_en.html>
+"""
+
 region = network.createRegion()
 display.setVisibleShape(region, shapes['Ball'](tessellation = 4))
 display.setVisibleColor(region, [1.0, 1.0, 1.0])
