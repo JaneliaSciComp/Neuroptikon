d4e6182ffe1c39f9fda318b5c249410fe1b9569e
Charlotte Weaver <weaverc10@janelia.hhmi.org>
2015-02-05 10:10:28 -0500

    Next and previous buttons for neuron image inspector

--- Source/Inspectors/NeuronImage/neuron_image_inspector.py
+++ Source/Inspectors/NeuronImage/neuron_image_inspector.py
@@ -29,13 +29,17 @@ class NeuronImageInspector( Inspector ):
     def window(self, parentWindow=None):
         if not hasattr(self, '_window'):
             self._window = wx.Window(parentWindow, wx.ID_ANY)
-            self.gridSizer = wx.FlexGridSizer(3, 1, 8, 8)
+            self.gridSizer = wx.FlexGridSizer(2, 1, 8, 8)
             self._currentIdx = 0
-            self._next = wx.Button(self._window, wx.ID_ANY, gettext('Next'))
             self._labelForNeuron = wx.StaticText(self._window, wx.ID_ANY, gettext(''))
+            
+
+            labelSizer = wx.BoxSizer(wx.HORIZONTAL)
+            labelSizer.Add(self._labelForNeuron, 1, wx.LEFT, 2)
+            
             self._imageOfNeuron = wx.StaticBitmap(self._window, wx.ID_ANY)
-            self.gridSizer.Add(self._labelForNeuron, 0, wx.EXPAND)
-            self.gridSizer.Add(self._next, 0, wx.LEFT, 8)
+            #self.gridSizer.Add(self._labelForNeuron, 0, wx.EXPAND)
+            self.gridSizer.Add(labelSizer)
             self.gridSizer.Add(self._imageOfNeuron, 0, wx.EXPAND)
             
             mainSizer = wx.BoxSizer(wx.VERTICAL)
@@ -57,19 +61,61 @@ class NeuronImageInspector( Inspector ):
                 return False #return only for first neuron selected
         return False
 
+    def _neuronImageCount(self, display):
+        selection = display.selection()
+        for visible in selection:
+            if visible.client is not None and visible.client.displayName() == 'Neuron':          
+                if visible.client.neuronImage:
+                    return len(visible.client.neuronImage)
+        return 0
+
     def inspectDisplay(self, display, imageIdx=0):
+        self.display = display
         image = self._neuronImage(display, imageIdx)
         self.gridSizer.Clear(True)
+
         if image and image.bmp is not None:
+            self.imageIdx = imageIdx
+            self.imageCount = self._neuronImageCount(display)
             windowSize = self._window.Parent.GetSize()
             self._labelForNeuron = wx.StaticText(self._window, wx.ID_ANY, gettext(image.label))
+
+            labelSizer = wx.BoxSizer(wx.HORIZONTAL)
+            labelSizer.Add(self._labelForNeuron, 1, wx.LEFT, 2)
+
+            if (self._neuronImageCount(display) > 1):
+                self._prevButton = wx.Button(self._window, wx.ID_ANY, gettext('Prev'), style = wx.BU_EXACTFIT)
+                self._prevButton.SetWindowVariant(wx.WINDOW_VARIANT_SMALL)
+                self._prevButton.SetSize(wx.Size(60, self._prevButton.GetSize().GetHeight()))
+                self._prevButton.SetMinSize(self._prevButton.GetSize())
+                self._window.Bind(wx.EVT_BUTTON, self._displayPrev, self._prevButton)
+                labelSizer.Add(self._prevButton, 0, wx.LEFT, 8)
+
+                self._nextButton = wx.Button(self._window, wx.ID_ANY, gettext('Next'), style = wx.BU_EXACTFIT)
+                self._nextButton.SetWindowVariant(wx.WINDOW_VARIANT_SMALL)
+                self._nextButton.SetSize(wx.Size(60, self._nextButton.GetSize().GetHeight()))
+                self._nextButton.SetMinSize(self._nextButton.GetSize())
+                self._window.Bind(wx.EVT_BUTTON, self._displayNext, self._nextButton)
+                labelSizer.Add(self._nextButton, 0, wx.LEFT, 8)   
+
             scaledImage = image.bmp.Rescale(windowSize[0], windowSize[1], wx.IMAGE_QUALITY_HIGH)
             self._imageOfNeuron = wx.StaticBitmap(self._window, wx.ID_ANY)
             self._imageOfNeuron.SetBitmap(wx.BitmapFromImage(scaledImage))
-            self.gridSizer.Add(self._labelForNeuron, 0, wx.EXPAND)
+            self.gridSizer.Add(labelSizer)
+            #self.gridSizer.Add(self._labelForNeuron, 0, wx.EXPAND)
+            #self.gridSizer.Add(self._next, 0, wx.EXPAND)
             self.gridSizer.Add(self._imageOfNeuron, 0, wx.EXPAND)
             self._currentIdx = imageIdx
         else:
             pass
         self._window.Layout()
+
+    def _displayNext(self, event):
+        nextImage = (self.imageIdx + 1) % self.imageCount
+        self.inspectDisplay(self.display, nextImage)
+
+    def _displayPrev(self, event):
+        prevImage = (self.imageIdx + 1) % self.imageCount
+        self.inspectDisplay(self.display, prevImage)
+
    
